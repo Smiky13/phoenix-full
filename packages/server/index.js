5d7b1b1b4a6dce0ed0af4142988ae91f4e07a490
Smiky13 <mikl2hocman@hotmail.com>
2025-10-15 22:08:37 +0200

    Update index.js

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
-app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors()); // on durcira plus tard
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
