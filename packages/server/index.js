167d4f05c2bd32d035f7eb089db30d6160c973b5
Smiky13 <mikl2hocman@hotmail.com>
2025-10-15 12:50:10 +0200

    Update index.js
    Ajout /health + CORS

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
 const PORT = process.env.PORT || 3000;
-
-app.get("/health", (req, res) => res.json({ status: "OK" }));
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
